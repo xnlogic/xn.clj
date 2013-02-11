d440486e75a5a9e6aea1d8576d6c1002f0cbb008
Darrick Wiebe <dw@xnlogic.com>
2013-02-11 00:24:33 -0500

    Test appears to work ok

--- src/lm/import/dc_subnets.clj
+++ src/lm/import/dc_subnets.clj
@@ -2,7 +2,17 @@
   (:require [xn.client :as xn]
             [xn.import :as i]
             [clojure.data.json :as json]
-            [clojure.string :as s]))
+            [clojure.string :as s]
+            [clojure.pprint :as pp]
+            [xn.repl :refer [prident]]))
+
+(defn info [name [id valn record :as result]]
+  (if id
+    (println (str "Executed " name ": id " id " (" (count valn) " errors)"))
+    (do
+      (println (str name " did not get an id:"))
+      (pp/pprint result)))
+  result)
 
 (defn dcs [filename]
   (json/read-str (slurp filename) :key-fn keyword))
@@ -36,26 +46,26 @@
 ;
 
 (defn create-subnet [{:keys [id subnet name direction notes]} zone-id]
-  (xn/execute {:method :put :url "model/subnet"
+  (info :create-subnet (xn/execute {:method :put :url "model/subnet"
                :body {:name name
                       :network_address subnet
                       :direction direction
                       :zones {:set [zone-id]}
                       :notes (str "Imported from NMDB ID: " id
-                                  (when (not= "" notes) (str " NMDB Notes: " notes)))}}))
+                                  (when (not= "" notes) (str " NMDB Notes: " notes)))}})))
 
 (defn update-subnet [{:keys [id name direction notes]} zone-id existing]
-  (xn/execute {:method :patch :url (str "model/subnet/" (:id existing))
+  (info :update-subnet (xn/execute {:method :patch :url (str "model/subnet/" (:id existing))
                :body {
                       ; TODO: uncomment these lines if you want to replace the existing name or direction with NMDB data
                       ; :name name
                       ; :direction direction
                       :zones {:set [zone-id]}
                       :notes (str (:notes existing) "; Updated from NMDB ID: " id
-                                  (when (not= "" notes) (str " NMDB Notes: " notes)))}}))
+                                  (when (not= "" notes) (str " NMDB Notes: " notes)))}})))
 
 (defn create-vlan [{:keys [direction notes vlan_type vlan primary_vlan id]} zone-id]
-  (xn/execute {:method :put :url "model/vlan"
+  (info :create-vlan (xn/execute {:method :put :url "model/vlan"
                :body {
                       ; TODO: is vlan the name? It's always just the vlan number.
                       :name vlan
@@ -64,10 +74,13 @@
                       :direction direction
                       :vlan_type vlan_type
                       :notes (str "Imported from NMDB ID: " id
-                                  (when (not= vlan primary_vlan) (str " (Primary VLAN: " primary_vlan)))}}))
+                                  (when (not= vlan primary_vlan) (str " (Primary VLAN: " primary_vlan)))}})))
 
 (defn create-zone [{:keys [name id subnets vlans]} pod-id existing-subnets]
   (let [subnets (group-by (comp boolean existing-subnets :subnet) subnets)
+        _ (when-not (empty existing-subnets)
+            (pp/pprint subnets)
+            (throw (Exception. "stop")))
         sn-to-create (subnets false)
         sn-to-update (subnets true)
         [zone-id valn record]
@@ -86,65 +99,75 @@
 
 (defn create-pod [name dc-id zones subnets]
   (let [[pod-id valn record]
-        (xn/execute {:method :put :url "model/zone"
+        (info :create-pod (xn/execute {:method :put :url "model/zone"
                      :body {:name name
                             :parent_zone dc-id
                             :notes (str "Imported from NMDB (Zones: " (count zones)
                                         " VLANs: " (count (mapcat :vlans zones))
-                                        " Subnets: " (count (mapcat :subnets zones)) ")")}})]
+                                        " Subnets: " (count (mapcat :subnets zones)) ")")}}))]
     (doseq [zone zones]
       (create-zone zone pod-id subnets))))
 
 (defn create-dc [dc pods zones]
   (let [[id valn record]
-        (xn/execute {:method :put :url "model/datacenter"
+        (info :create-dc (xn/execute {:method :put :url "model/datacenter"
                      :body {:name (:name dc)
                             :notes (str "Imported from NMDB ID: " (:id dc)
                                         " (Pods: " (count pods)
                                         " Zones: " (count zones)
                                         " VLANs: " (count (mapcat :vlans zones))
-                                        " Subnets: " (count (mapcat :subnets zones)) ")")}})]
+                                        " Subnets: " (count (mapcat :subnets zones)) ")")}}))]
     id))
 
-(defn load-dc [dc]
+(defn load-dc [dc xnid-map]
   (let [xnid (:datacenter_xnid dc)
         xnid (when (not= "" xnid) xnid)
-        subnets (if xnid
-                  {}
-                  (->> (xn/get-vec (xn/join-url xnid "/rel/subnets/properties/network_address,notes"))
+        xnid (get xnid-map xnid xnid)
+        subnets (if (prident "xnid" xnid)
+                  (->> (xn/get-vec (prident "subnet info url" (xn/join-url xnid "/rel/subnets/model/subnet/properties/network_address,notes")))
                     (map (fn [[id subnet notes]] [subnet {:id id :notes notes}]))
-                    (into {})))
+                    (into {})
+                    (prident "subnet info"))
+                  (prident "no subnets" {}))
         zones (apply concat (:pods dc))
         pods (group-by :pod zones)
         dc-id (if xnid
-                (last (s/split #"/" xnid))
+                (last (s/split xnid #"/"))
                 (create-dc dc pods zones))]
     (doseq [[name zones] pods]
       (create-pod name dc-id zones subnets))))
 
 
-(defn load-all [dcs]
+(defn load-all [dcs xnid-map]
   (doseq [dc dcs]
-    (load-dc dc)))
+    (load-dc dc xnid-map)))
 
 ; Config before calling, if needed:
 ; (xn.client/setup :url "..." :token "...")
-(defn load! [filename]
-  (-> filename
-    dcs
-    load-all))
+(defn load!
+  ([filename]
+   (load! filename {}))
+  ([filename xnid-map]
+   (-> filename
+     dcs
+     (load-all xnid-map))))
 
 (defn run-test! [filename]
-  (when-not (re-find #"^sample" @*token)
+  (when-not (re-find #"^sample" @xn/*token)
     (throw (Exception. "Run the test into a sample app")))
-  (let [xnid (:datacenter_xnid dc)
-        xnid (when (not= "" xnid) xnid)]
+  (let [xnid-map (atom {})]
+    (doseq [dc (dcs filename)]
+      (let [xnid (:datacenter_xnid dc)
+            xnid (when (not= "" xnid) xnid)]
 
-    (when xnid
-      (let [[dc-id & _] (xn/execute {:method :put :url "model/datacenter"
-                                     :name (str "a test dc " xnid)})
-            zones (apply concat (:pods dc))
-            subnets (drop 1 (take 10 (mapcat :subnets zones)))]))
-    (doseq [subnet subnets]
-      (create-subnet subnet dc-id))
-    (load! filename)))
+        (when xnid
+          (let [[dc-id & _] (info :sample-dc (xn/execute {:method :put :url "model/datacenter"
+                                                          :name (str "a test dc " xnid)}))
+                zones (apply concat (:pods dc))
+                subnets (drop 1 (take 10 (mapcat :subnets zones)))]
+            (swap! xnid-map assoc xnid (str "/model/datacenter/" dc-id))
+            (doseq [subnet subnets]
+              (create-subnet subnet dc-id))))))
+    (println " -----------------------------------")
+    (println " -----------------------------------")
+    (load! filename @xnid-map)))
