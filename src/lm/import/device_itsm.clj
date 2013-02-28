a654ecef8c142db383f0cdb488f7d93fcd5a68e4
Darrick Wiebe <dw@xnlogic.com>
2013-02-28 00:03:27 -0500

    Map out the fields in the device-itsm data file

--- src/lm/import/device_itsm.clj
+++ src/lm/import/device_itsm.clj
@@ -1,30 +1,52 @@
-{:device_ciid :ciid ; look up by external record
- :device_name nil
- :device_class nil
- :incidents
- [{:status "closed",
-   :impact 2,
-   :class nil
-   :submitter "KLEPACMA",
-   :support_group "SERVER CI DATA",
-   :organization "MGS-ESC",
-   :short_description :description
-   :urgency 3,
-   :submit_date "2011-10-28T15:14:21-04:00",
-   :id :ciid
-   :priority 2}],
- :rfcs
- [{:status "closed",
-   :class "Remedy::Rfc",
-   :submitter "MARUTP",
-   :submitter_email nil,
-   :organization "MGS",
-   :service_category_tier_2 nil,
-   :service_category_tier_1 "Infrastructure Services",
-   :service_category_tier_3 nil,
-   :short_description
-   "Upgrading listed ETC  Servers to Windows 2003 SP2- July 12",
-   :submitter_full_name "Paul Marut",
-   :submit_date "2009-06-26T13:21:38-04:00",
-   :id :ciid
-   :project nil}]}
+(ns lm.import.device_itsm
+  (:require [xn.client :as xn]
+            [xn.import :as i :refer [extract-records create-unique set-one-rels add-many-rels]]
+            [clojure.string :as s]
+            [xn.tools :refer [vectorize]]))
+
+(defn incident-records [records]
+  (extract-records
+    {:status :status
+     :impact :impact
+     :class nil
+     :submitter :submitter
+     :support_group :project
+     :organization :customer
+     :short_description :description
+     :urgency :urgency
+     :submit_date :submission_date
+     :id :ciid
+     :priority :priority}
+    records))
+
+(defn rfc-records [records]
+  (extract-records
+    {:status :status
+     :class nil
+     :submitter :submitter
+     :submitter_email nil
+     :organization :customer
+     :service_category_tier_1 :sc
+     :service_category_tier_2 :sc
+     :service_category_tier_3 :sc
+     :short_description :description
+     :submitter_full_name nil
+     :submit_date :submission_date
+     :id :ciid
+     :project :project}
+    records))
+
+(defn device-records [records]
+  (extract-records
+    {:incidents incident-records
+     :rfcs rfc-records}
+    {:sc vectorize}
+    {:device_ciid :ciid ; look up by external record
+     :device_name nil
+     :device_class nil
+     :incidents :incidents,
+     :rfcs :rfcs}
+    records))
+
+
+
